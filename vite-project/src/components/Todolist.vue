b653d473b70e8f1bfbb76e9870d79f9877240b4d
dujuan <jammydu@qq.com>
2022-01-13 19:24:16 +0800

    新增Todolist.vue

---
+++ vite-project/src/components/Todolist.vue
@@ -0,0 +1,19 @@
+
+<template>
+  <div>
+    <h1 @click="add">{{count}}</h1>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+let count = ref(1)
+function add(){
+    count.value++
+}
+</script>
+
+<style>
+  color: red;
+}
+</style>
